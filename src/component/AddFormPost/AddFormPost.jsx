87f8806a5f0058cc1a73c81f54544172ec02715e
Bogdan <bogdankozlovski18@gmail.com>
2021-11-09 05:52:38 +0200

    13_lesson

--- src/component/AddFormPost/AddFormPost.jsx
+++ src/component/AddFormPost/AddFormPost.jsx
@@ -2,26 +2,59 @@ import React from 'react';
 import CloseIcon from '@mui/icons-material/Close';
 import './addFormPost.css'
 
-const AddFormPost = ({handleFormAddClose}) => {
-    return (
-        <div className="form__post">
+class AddFormPost extends React.Component {
 
-            <form className="form">
-                <button className="form__close_btn">
-                    <CloseIcon onClick={handleFormAddClose}/>
-                </button>
-                <h1 className="form__title">
-                    Создание поста
-                </h1>
-                <input className="form__input" type="text" placeholder={'Заголовок поста'}/>
-                <textarea className="form__textarea" placeholder={'Описание поста'}/>
-                <button onClick={handleFormAddClose} className="form__btn" type='button'>
-                    Создать пост
-                </button>
-            </form>
-            <div onClick={handleFormAddClose} className="form__overlay"></div>
-        </div>
-    );
-};
+    state = {
+        postTitle: '',
+        postDescr: ''
+    }
+
+    menagePostTitleChange = (e) => {
+        this.setState({
+            postTitle: e.target.event
+        })
+    }
+
+    menagePostDescrChange = (e) => {
+        this.setState({
+            postDescr: e.target.event
+        })
+    }
+
+
+    render() {
+        const handleFormAddClose = this.props.handleFormAddClose;
+        console.log(this.state)
+        return (
+            <div className="form__post">
+
+                <form className="form">
+                    <button className="form__close_btn">
+                        <CloseIcon onClick={handleFormAddClose}/>
+                    </button>
+                    <h1 className="form__title">
+                        Создание поста
+                    </h1>
+                    <input
+                        value={this.state.postTitle}
+                        onChange={this.menagePostTitleChange}
+                        className="form__input"
+                        type="text"
+                        placeholder={'Заголовок поста'}/>
+                    <textarea
+                        value={this.state.postDescr}
+                        onChange={this.menagePostDescrChange}
+                        className="form__textarea"
+                        placeholder={'Описание поста'}/>
+                    <button onClick={handleFormAddClose} className="form__btn" type='button'>
+                        Создать пост
+                    </button>
+                </form>
+                <div onClick={handleFormAddClose} className="form__overlay"></div>
+            </div>
+        );
+
+    }
+}
 
 export default AddFormPost;
